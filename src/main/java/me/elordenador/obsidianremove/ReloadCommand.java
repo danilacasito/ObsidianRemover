201e903f19499aa4a9b50d23e12e7591c6ca8a04
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2022-11-14 15:29:10 +0100

    Create basic listener for /obreload

---
+++ src/main/java/me/elordenador/obsidianremove/ReloadCommand.java
@@ -0,0 +1,12 @@
+package me.elordenador.obsidianremove;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ReloadCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
