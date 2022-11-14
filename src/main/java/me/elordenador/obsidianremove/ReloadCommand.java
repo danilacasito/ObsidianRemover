e951261701586da650acf30d3a8193d40dd2020c
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2022-11-14 15:31:00 +0100

    Implement code for obreload

--- src/main/java/me/elordenador/obsidianremove/ReloadCommand.java
+++ src/main/java/me/elordenador/obsidianremove/ReloadCommand.java
@@ -3,10 +3,16 @@ package me.elordenador.obsidianremove;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class ReloadCommand implements CommandExecutor {
+    JavaPlugin plugin;
+    public ReloadCommand(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+        this.plugin.reloadConfig();
+        return true;
     }
 }
